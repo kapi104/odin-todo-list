8d1ad1af14f66d47feb1ef84dead4f14087fbe88
kapi104 <skorupa7979@gmail.com>
2023-08-26 14:14:03 +0200

    fixed animation delay, requiers cubic-bezier tweaks

--- src/generateTodo.js
+++ src/generateTodo.js
@@ -6,24 +6,65 @@ const generateList = () => {
 
 }
 
-const generateNewTodoForm = () => {
-  
+const newCaption = (text) => {
+  let caption = document.createElement('caption');
+  caption.innerText = text;
+  return caption;
+}
+
+const newInput = (type, inputClass, blockEnter, max) => {
+  let input = document.createElement('input');
+  input.setAttribute('type', type)
+  input.classList.add(inputClass);
+  if (blockEnter) {
+    input.setAttribute('onkeydown', "return event.key != 'Enter';")
+  }
+  input.setAttribute('maxlength', max)
+}
+
+const generateNewTodoForm = (element) => {
+  const formContainer = document.createElement('div');
+  formContainer.classList.add('newTodoForm')
+
+  const form = document.createElement('form')
+
+  form.append(
+    newCaption('Task:'),
+    newInput('text', 'nTask', true, 40),
+    newCaption('Description:'),
+    newInput('textarea', 'ndescription', false, 500),
+    newCaption('Due date:'),
+    newInput('date', 'nDueDate', true, ''),
+  )
+
+  formContainer.appendChild(form)
+
+  element.appendChild(formContainer)
+}
+
+const showTodoContainer = (element) => {
+  window.getComputedStyle(element).maxHeight;
+  element.classList.add('todoShown')
 }
 
 const generateTodoContainer = (project) => {
-  
   const element = document.createElement('div');
   element.classList.add('todoContainer');
   project.appendChild(element)
-  window.getComputedStyle(element).maxHeight;
-  element.classList.add('todoShown')
+
+  generateNewTodoForm(element)
   
-    
-  
+  showTodoContainer(element)
 }
 
 const removeTodoContainer = (project) => {
-  project.querySelector('.todoContainer').addEventListener('transitionend', (e) => {e.target.remove()})
+  project.querySelector('.todoContainer').addEventListener('transitionend', (e) => {
+    if (e.propertyName == 'max-height') {
+      console.log(e.propertyName);
+      e.target.remove()
+    }
+    
+  })
     
   project.querySelector('.todoContainer').classList.remove('todoShown');
 }
