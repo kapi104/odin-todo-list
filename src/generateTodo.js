1b6fb4e99cf417f91736d1bf74f491a9d0c618bc
kapi104 <skorupa7979@gmail.com>
2023-08-26 12:59:53 +0200

    added animation to todo container

--- src/generateTodo.js
+++ src/generateTodo.js
@@ -1,5 +1,3 @@
-import { rotateArrow } from "./eventHandlers";
-
 const loadTodoFromLocalStorage = (ID) => {
   
 }
@@ -12,23 +10,34 @@ const generateNewTodoForm = () => {
   
 }
 
-const removeEventListeners = (element) => {
-  element.replaceWith(element.cloneNode(true));
+const generateTodoContainer = (project) => {
+  
+  const element = document.createElement('div');
+  element.classList.add('todoContainer');
+  project.appendChild(element)
+  window.getComputedStyle(element).maxHeight;
+  element.classList.add('todoShown')
+  
+    
+  
 }
 
-const generateTodoContainer = (project, projectID) => {
+const removeTodoContainer = (project) => {
+  project.querySelector('.todoContainer').addEventListener('transitionend', (e) => {e.target.remove()})
+    
+  project.querySelector('.todoContainer').classList.remove('todoShown');
+}
+
+const containerCheck = (project, projectIDID) => {
   if(project.querySelector('.todoContainer') == null) {
-    const element = document.createElement('div');
-    element.classList.add('todoContainer', 'todoHidden');
-    project.appendChild(element)
-    removeEventListeners(project.querySelector('.todoContainer'))
+    generateTodoContainer(project)
   } else {
-    project.querySelector('.todoContainer').remove();
+    removeTodoContainer(project)
   }
 }
 
 const createTodoList = (project, projectID) => {
-  generateTodoContainer(project, projectID)
+  containerCheck(project, projectID)
 }
 
 export default createTodoList;
