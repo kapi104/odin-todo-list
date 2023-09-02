afb900f18030e01934427e4040ee595c350a75f5
kapi104 <skorupa7979@gmail.com>
2023-09-02 16:05:02 +0200

    created custom radio buttons for priority selection

--- src/generateTodo.js
+++ src/generateTodo.js
@@ -22,12 +22,14 @@ const generateSingleTodo = (todoObject, project, insertPriority) => {
   header.classList.add('header')
   const priority = document.createElement('div');
   priority.classList.add('priority');
-  priority.innerHTML = todoObject.priority;
   if (todoObject.priority == '0') {
+    priority.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>star-outline</title><path d="M12,15.39L8.24,17.66L9.23,13.38L5.91,10.5L10.29,10.13L12,6.09L13.71,10.13L18.09,10.5L14.77,13.38L15.76,17.66M22,9.24L14.81,8.63L12,2L9.19,8.63L2,9.24L7.45,13.97L5.82,21L12,17.27L18.18,21L16.54,13.97L22,9.24Z" /></svg>';
     priority.classList.add('priority0')
   } else if (todoObject.priority == '1') {
+    priority.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>star</title><path d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z" /></svg>'
     priority.classList.add('priority1')
   } else if (todoObject.priority == '2') {
+    priority.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>star</title><path d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z" /></svg>'
     priority.classList.add('priority2')
   }
 
@@ -56,12 +58,17 @@ const generateSingleTodo = (todoObject, project, insertPriority) => {
 
   if (insertPriority != undefined) {
     const priorityElements = project.querySelectorAll(`.priority${insertPriority}`);
-    const lastElementWithPrio = priorityElements[priorityElements.length - 1];
+
+    if (priorityElements.length != 0) {
+      const lastElementWithPrio = priorityElements[priorityElements.length - 1];
 
     const parentTodo = lastElementWithPrio.parentElement.parentElement.parentElement
 
     parentTodo.insertAdjacentElement('afterend', todo)
-    
+    } else {
+      project.querySelector('.todoList').appendChild(todo)
+    }
+
   } else {
     project.querySelector('.todoList').appendChild(todo)
   }
@@ -113,9 +120,8 @@ const generatePriority = () => {
   priorityContainer.classList.add('priorityContainer');
   
   for(let i = 0; i < 3; i++) {
-    const radioContainer = document.createElement('div')
-    radioContainer.innerHTML = i
-
+    const radioContainer = document.createElement('label')
+    radioContainer.classList.add('radioLabel')
     const radio = document.createElement('input');
     if (i === 0) {
       radio.checked = true;
@@ -125,7 +131,21 @@ const generatePriority = () => {
     radio.setAttribute('name', 'priority')
     radio.value = i
 
-    priorityContainer.appendChild(radioContainer).appendChild(radio)
+    const radioSpan = document.createElement('span')
+    radioSpan.classList.add('radioSpan');
+    if (i == 0) {
+      radioSpan.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>star-outline</title><path d="M12,15.39L8.24,17.66L9.23,13.38L5.91,10.5L10.29,10.13L12,6.09L13.71,10.13L18.09,10.5L14.77,13.38L15.76,17.66M22,9.24L14.81,8.63L12,2L9.19,8.63L2,9.24L7.45,13.97L5.82,21L12,17.27L18.18,21L16.54,13.97L22,9.24Z" /></svg>'
+      radioSpan.classList.add('priorityRadio0')
+    } else if (i == 1) {
+      radioSpan.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>star</title><path d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z" /></svg>'
+      radioSpan.classList.add('priorityRadio1')
+    } else if (i == 2) {
+      radioSpan.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>star</title><path d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z" /></svg>'
+      radioSpan.classList.add('priorityRadio2')
+    }
+    
+
+    priorityContainer.appendChild(radioContainer).append(radio, radioSpan)
   }
   return priorityContainer;
 } 
